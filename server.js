fd5dc187c733f3f5915fbb68f633426f0eaaf161
petro <36398735+PetroSutch@users.noreply.github.com>
2018-07-03 15:12:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,50 @@
 var express = require('express');
+var path = require('path');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var server = require('http').Server(app);
+var io = require('socket.io')(server,{ wsEngine: 'ws' });
 
-app.get('/', function(req, res, next) {
-	res.sendFile(__dirname + 'index.html')
-});
+var port = 3000;
 
-//app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "public")));
+var users = {};
+var listUser = [];
+io.on('connection', (socket) => {
 
-var j = 0;
-
-io.sockets.on('connection', function(client) {
-	console.log('Client connected...');
-	client.user_id = 1;
-	client.on('join', function(data) {
-		console.log(data);
-			console.log(io.sockets.connected)
+	socket.on('addUser', (name_user) => {
+		socket.id_user = name_user;
+		users[name_user] = socket.id
+		var user = {'name':name_user,'socket_id':socket.id};
+		listUser.push(user);
+		io.sockets.emit('listUser',listUser);
 	});
-
-
-
-	client.on('messages', function(data){
-		//client.emit('thread', data);
-		//client.broadcast.emit('thread', data);
 	
-	//	.client('1').emit(data);
+
+	socket.on('send-message', (msg) => {
+		if( io.sockets.connected[msg['id2']]){
+			io.sockets.connected[msg['id2']].emit('send-message', {msg:msg.message});
+		}else{
+			console.log('2')
+		}
 	});
+
+
+
+
+	socket.on('disconnect', (data) => {
+		for(var i in users){
+			if(users[i] == socket.id){
+				console.log(i)
+				delete users[i];
+			}
+		}
+		io.sockets.emit('listUser', users);
+	});
+
 });
 
-server.listen(7777);
+
+
+server.listen(port, () => {
+  console.log("Listening on port " + port);
+});
