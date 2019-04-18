687af4250a47cd911d054012ede813529fa7fb1e
PetroSuch <p.such@nltu.lviv.ua>
2019-04-18 12:36:12 +0300

    push chat

--- client.js
+++ client.js
@@ -1,19 +1,122 @@
-         // initializing socket, connection to server
-         //var socket = io.connect('http://localhost:7777');
-         socket.on('connect', function(data) {
-            socket.emit('join', 'Hello server from client');
-         });
+//956107799194-vnbcq8gk3efjtjo5g28rijknu4o8tnsv.apps.googleusercontent.com
+//4kW__Hun48X6tqzfohUMC8p_
 
-         // listener for 'thread' event, which updates messages
-         socket.on('thread', function(data) {
-            $('#thread').append('<li>' + data + '</li>');
-         });
+const socket = io.connect('http://localhost:3000');
+const url = new URL(document.location); 
 
-         // prevents form from submitting and sends a message to server
-         $('form').submit(function(){
-            var message = $('#message').val();
-            console.log(message)
-            socket.emit('messages', message);
-            this.reset();
-            return false;
-         });
+console.log(localStorage)
+function onSignIn(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	var obj = {'data_type':'signin','name':profile.getName(),'email':profile.getEmail(),'image': profile.getImageUrl(),'lang':$('#lang').val()}
+	socket.emit('signin',obj)
+}
+$(document).ready(()=>{
+	if(localStorage.getItem('lang') != null){
+		$('#lang').find('option[value="'+localStorage.getItem('lang')+'"]').prop('selected','selected')
+	}
+	
+	/*localStorage.removeItem('id_user');
+   localStorage.removeItem('id_chat');
+   localStorage.removeItem('url_chat');
+   console.log(localStorage)
+   return false ;*/
+	if(localStorage.getItem('id_user') == null){
+		$('#chat').hide()
+		$('.g-signin2').show()
+	}else{
+		$('#chat').show()
+		$('.g-signin2').hide()
+	}
+	socket.on('connect', function(data) {
+		if(localStorage.getItem('id_user') != null && url.searchParams.get('url_chat') == null){
+			socket.emit('client_new_user',{'lang':$('#lang').find('option:selected').val(),'id_user':localStorage.getItem('id_user')});
+		}else if(localStorage.getItem('id_user') != null){
+			socket.emit('client_new_user',{'id_chat':localStorage.getItem('id_chat'),'url_chat':url.searchParams.get('url_chat'),'id_user':localStorage.getItem('id_user'),'lang':$('#lang').find('option:selected').val()});
+		}
+	});
+    socket.on('user_is_signin',function(data){
+		localStorage.setItem('id_user',data.id_user)
+		$('#chat').show()
+		$('.g-signin2').hide()
+		if(localStorage.getItem('id_user') != null && url.searchParams.get('url_chat') == null){
+			socket.emit('client_new_user',{'lang':$('#lang').find('option:selected').val(),'id_user':localStorage.getItem('id_user')});
+		}else{
+			socket.emit('client_new_user',{'id_chat':localStorage.getItem('id_chat'),'url_chat':url.searchParams.get('url_chat'),'id_user':localStorage.getItem('id_user'),'lang':$('#lang').find('option:selected').val()});
+		}
+    })
+   socket.on('server_new_chat_data', function(data) {
+      localStorage.setItem('id_chat',data.id_chat)
+      localStorage.setItem('url_chat',data.url_chat)
+      localStorage.setItem('id_user',data.id_user)
+      localStorage.setItem('lang',data.lang)
+      insertParam('url_chat',localStorage.getItem('url_chat'))
+      loadMsgChat(socket)
+   });
+
+   socket.on('server_get_chat_msg', function(data) {
+       $('#msg').html('')
+       var lang = localStorage.getItem('lang') != null? localStorage.getItem('lang'):$('#lang').val()
+		$.each(data,(k,v)=>{
+			console.log(k)
+			var msg = $('#msg_html').find('#from').eq(0).clone()
+			if(data[k]['msg_from'] == localStorage.getItem('id_user')){
+				msg = $('#msg_html').find('#my').eq(0).clone()
+			}
+			var msgTxt = JSON.parse(data[k].msg);
+				msgTxt = msgTxt[lang]
+			msg.removeAttr('style').find('p').text(msgTxt)
+			$('#msg').prepend(msg)
+		})
+   });
+
+   socket.on('server_new_msg',(data)=>{
+   	console.log(data)
+	var msg = $('#msg_html').find('#from').eq(0).clone()
+		if(data['msg_from'] == localStorage.getItem('id_user')){
+			msg = $('#msg_html').find('#my').eq(0).clone()
+		}
+		$.each(data['msg'],function(k,v){
+
+		if(k == $("#lang").val()){
+			msg.removeAttr('style').find('p').text(data['msg'][k])
+		}
+	})
+	msg.find('.time-right').text(data['date'])
+	$('#msg').prepend(msg)
+   })
+    socket.on('test_emit',(data)=>{
+      console.log(data)
+    })
+   $('#send').click(function(event){
+      if($('#message').val().length > 1 ){
+         socket.emit('client_send_new_msg',{"date":dateTime(),"id_user":localStorage.getItem('id_user'),'url_chat':localStorage.getItem('url_chat'),'msg':$("#message").val(),'lang':$('#lang option:selected').attr('value')})
+         $('#message').val('')
+      }
+   })
+   $('#message').keypress(function (e) {
+     if (e.which == 13 && $('#message').val().length > 1) {
+        socket.emit('client_send_new_msg',{"date":dateTime(),"id_user":localStorage.getItem('id_user'),'url_chat':localStorage.getItem('url_chat'),'msg':$("#message").val(),'lang':$('#lang option:selected').attr('value')})
+        $('#message').val('')
+     }
+   });
+   $('#lang').on('change',function(){
+   	 localStorage.setItem('lang',$(this).val())
+      socket.emit('client_change_lang',{id_user:localStorage.getItem('id_user'),lang:$(this).val()})
+   })
+   
+})
+
+function dateTime(){
+   var d = new Date();
+   return d.getHours()+':'+d.getMinutes()
+}
+
+function loadMsgChat(socket){
+   socket.emit('client_load_chat_msg',{"id_chat":localStorage.getItem('url_chat'),"url_chat":localStorage.getItem('url_chat')})
+}
+
+function insertParam(key, value){
+    const params = new URLSearchParams(location.search);
+    params.set(key, value);
+    window.history.replaceState({}, "", decodeURIComponent(`${location.pathname}?${params}`));
+}
--- server.js
+++ server.js
@@ -6,13 +6,12 @@ var io = require('socket.io')(server,{ wsEngine: 'ws' });
 var mysql = require('mysql');
 
 var port = process.env.PORT || 3000;
-
-
+var googleTranslate = require('google-translate')('AIzaSyDrn4uhUxn2Jk8HopmbtBsDe4q3ILC5LgE');
 var con = mysql.createConnection({
-  host: "qumel.mysql.tools",
-  user: "qumel_myspace",
-  password: "8ng7bd6u",
-  database: "qumel_myspace"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "chat"
 });
 
 con.connect(function(err) {
@@ -21,407 +20,120 @@ con.connect(function(err) {
 });
 
 
-var uri = "mongodb://root:aezakmi1@ds235711.mlab.com:35711/kiril";
-var options = {
-	useNewUrlParser: true 
-};
-var mongoose = require("mongoose");
-	mongoose.connect(uri,options);
-
-
-var Schema =  mongoose.Schema;
-
-var msgScheme = new Schema({
-	id_dialog:String,
-    msg: String,
-    read:Boolean,
-    edit:Boolean,
-    id_1:Number,
-	id_2:Number,
-	id_author:Number,
-	user_1:Object,
-	user_2:Object,
-	time:String,
-	date:String,
-	file:Boolean
-},{timestamps: true});
-
-
-var dialogSchema = new Schema({
-	id_1:Number,
-    id_2: Number,
-    user1: Object,
-    user2:Object
-},{timestamps: true});
-
-var msgModel = mongoose.model("messages", msgScheme);
-var dialogModel = mongoose.model("dialog", dialogSchema);
-
-var newMessageModel = new msgModel();
-var newDialogModel = new dialogModel();
-
-
 
 app.use(express.static(path.join(__dirname, "public")));
 server.listen(port, () => {
   console.log("Listening on port " + port);
 });
-
-function findUserSocketById(id_user){}
-
-var users = {};
-var listSocket = {};
-var listDisconnect = {};
 io.on('connection', (socket) => {
-
-	socket.on('check_online_list_dialog', (obj)=>{
-		var listDialog = obj['listDialog']
-		for(var k in listDialog){
-			var id_2 = obj['id_user'] == listDialog[k]['dialog']['id_1']?listDialog[k]['dialog']['id_2']:listDialog[k]['dialog']['id_1']
-			if(users[id_2]){
-				listDialog[k]['online'] = true
+	socket.on('signin',(data)=>{
+		var sql = "SELECT * FROM  users WHERE email='"+data.email+"'";
+		con.query(sql, function (err, result) {
+			if(result.length == 0){
+				var sql2 = "INSERT INTO users (name,email,image,lang) VALUES ('"+data.name+"','"+data.email+"','"+ data.image+"','"+ data.lang+"')";
+				con.query(sql2, function (err2, result2) {
+					if (err2) throw err2;
+					socket.id_user = result2.insertId
+					socket.emit('user_is_signin',{"id_user":result2.insertId})
+				})
 			}else{
-				listDialog[k]['online'] = false
+				socket.emit('user_is_signin',{"id_user":result[0].id})
 			}
-		}
-
-		socket.emit('result_check_online_list_dialog',listDialog)
-	})
-
-	socket.on('check online', (obj)=>{
-		if(users[obj['id_2']]){
-			socket.emit('result check online',{bool:true,time:''})
-		}else{
-			var time = false;
-			if(listDisconnect[obj['id_2']]){
-				time = timeSince(listDisconnect[obj['id_2']]);
-			}
-			socket.emit('result check online',{bool:false,time:time})
-		}
-	})
-
-	
-	socket.on('find list dialog', (obj)=>{
-		console.log('find list dialog')
-		var result = [];
-		
-		dialogModel.find({$or: [{'id_1':obj['id_user']},{'id_2':obj['id_user']}]}, function (err, objFind) {
-			var arrId = [];
-			for(var key in objFind){
-				arrId.push(objFind[key]['_id'])
-			}
-			msgModel.find({
-			    'id_dialog': { $in: arrId}
-			})
-			.sort({'_id': -1})
-			.exec(function(err, msg) {
-				//mongoose.connection.close()
-				if(err){console.log(err)}
-				if (err) throw err;
-				for(var k in objFind){
-					var id_2 = obj['id_user'] == objFind[k]['id_1']?objFind[k]['id_2']:objFind[k]['id_1']
-
-					var push = {'dialog':objFind[k],'last_msg':'','count_unread':0,"date":"",}
-					
-					for(j in msg){
-						if(objFind[k]['_id'] == msg[j]['id_dialog'] ){
-							push['last_msg'] = msg[j];
-							push['date'] = msg[j]['createdAt'];
-							break;
-						}
-					}
-					for(j in msg){
-						var id_1 = msg[j]['id_1'] == obj['id_user'] ? msg[j]['id_2']:msg[j]['id_1'];
-						if(objFind[k]['_id'] == msg[j]['id_dialog'] && msg[j]['id_author'] == id_1  && msg[j]['read'] == false){
-							push['count_unread'] += 1;
-						}
-					}
-
-					result.push(push)	
-					socket.emit('list dialog',result);
-				}
-			});
-		
 		})
 	})
-
-	socket.on('add user',(obj)=>{
-		//if(!users[obj['id_1']]){
-			socket.id_1 = obj['id_1'];
-			var user1 = obj['user1'];
-			users[obj['id_1']] = {'id':obj['id_1'],'socket':socket.id,'user':user1,'id_dialog':''}
-			listSocket[socket.id] = obj['id_1'];
-			console.log('user is add '+users[obj['id_1']]['socket'])
-			console.log('-----')
-
-		/*else{
-			users[obj['id_1']]['socket'][socket.id]
-			listSocket[socket.id] = obj['id_1'];
-			console.log('user is update '+socket.id)
-
-
-		}*/
-
-	})
-
-	socket.on('set dialog id',(obj)=>{
-		if(users[obj['id_1']]){
-			users[obj['id_1']]['id_dialog'] = obj['id_dialog']
-		}
-	})
-
-	socket.on('load prev msg',(obj)=>{
-		console.log(obj)
-		msgModel.find({_id:{$lt: obj['id']}})
-				.sort({'_id':-1})
-				.limit(5)
-				.exec(function(err, msg) {
-					socket.emit('result load prev msg',msg)
-				})
-
-	})
-
-
-
-
-
-	socket.on('find dialog', (obj)=>{
-		console.log('find dialog')
-		var id_1 = obj['id_1']?obj['id_1']:null;
-		var id_2 = obj['id_2']?obj['id_2']:null;
-		dialogModel.findOne({$or: [{'id_1':id_1,'id_2':id_2},{'id_2':id_1,'id_1':id_2}]}, function (err, objFind) {
-			 /*mongoose.disconnect();*/
-			if(objFind){
-				socket.emit('result_find_dialog',objFind)
-
-				msgModel.find({'id_dialog':objFind['_id']})
-				.sort({'_id':-1})
-				.limit(10)
-				.exec(function(err, msg) {
-					var res = msg.reverse()
-					if (err) throw err;
-				    socket.emit('load_old_message',res);
+	socket.on('client_new_user',(data)=>{
+		if(data.id_user != null && data.url_chat == null){
+			/*додаєм юзера в діалог*/
+			var url_chat = new Date().getTime(); 
+				socket.join(url_chat);
+			var sql = "INSERT INTO chats (url) VALUES ('"+url_chat+"')";
+			con.query(sql, function (err, result) {
+				var sql_update = "UPDATE users SET room = '"+url_chat+"' WHERE id ="+data.id_user;
+				con.query(sql_update, function (err2, result2) {
+					if (err2) throw err2;
+					socket.emit('server_new_chat_data',{"url_chat":url_chat,"id_chat":result.insertId,"id_user":data.id_user,'lang':result2.lang})
 				});
-			}else{	
-				con.query("SELECT id,name,phone,img,date_edit FROM user WHERE id IN ("+id_1+","+id_2+")", function (err, result, fields) {
-					
-					if(err) console.log(err);				
-					if(result){
-						var newDialogModel = new dialogModel();
-						dialogModel.create({
-							id_1:result['0']['id'],
-							id_2:result['1']['id'],
-							user1: result['0'],
-							user2: result['1']
-							}, function (err, small) {
-							if (err){ return err}
-							if(users[obj['id_2']]){
-								var socketUser2 = users[obj['id_2']];
-								try {
-									io.sockets.connected[socketUser2['socket']].emit('refresh list dialog', {bool:true} );
-								}catch(err){
-								//	console.log(err)
-								}
-							}
-							dialogModel.findOne({$or: [{'id_1':obj['id_1'],'id_2':obj['id_2']},{'id_2':obj['id_1'],'id_1':obj['id_2']}]}, function (err, objFind) {
-								socket.emit('result_find_dialog',objFind)
-								msgModel.find({'id_dialog':objFind['_id']})
-								.sort({'_id':-1})
-								.limit(10)
-								.exec(function(err, msg) {
-									var res = msg.reverse()
-									 /*mongoose.disconnect();*/
-									if (err) throw err;
-								    socket.emit('load_old_message',res);
-								});
-							})
-						});
-					}
-				//	con.end();
-				});
-			}
-
-		});
-		
-	})
-	socket.on('typing', (obj) => {
-		if(typeof users[obj['id_2']] != 'undefined' && obj['id_2'] != null){
-			var socketUser2 = users[obj['id_2']];
-			var objSend = {'typingBool':obj['typing'],'id_dialog':obj['id_dialog']};
-			try {
-				io.sockets.connected[socketUser2['socket']].emit('is_typing', objSend );
-			}catch(err){
-			//	console.log(err)
-			}
-		}
-	})
-	socket.on('edit message', (obj)=>{
-		msgModel.update({_id: obj['id_msg']}, {msg: obj['msg'],edit:true}, function(err, result){
-		    if(err)  console.log(err);
-		    if(result){
-		    	socket.emit('new edit message',obj)
-		    	if( users[obj['id_2']] ){
-					var socketUser2 = users[obj['id_2']];
-					try {
-						io.sockets.connected[socketUser2['socket']].emit('new edit message', obj);
-					}catch(err){
-					//	console.log(err)
-					}
-				}
-		    }
-		});
-	})
-
-	socket.on('delete msg', (obj)=>{
-		/*msgModel.findOne({id_dialog:obj['id_dialog'],_id:obj['id_msg']}, function(err, result){
-		    if(err)  console.log(err);
-		    if(result){
-		    	if(result['id_author'] == obj['id_user']){
-		    		check is user message
-		    	}	
-		    }
-		});*/
-
-		msgModel.remove({_id:obj['id_msg']}, function(err, result){
-			if(err) console.log(err)
-			if(result){
-				socket.emit('new delete message',obj)
-		    	if( users[obj['id_2']] ){
-					var socketUser2 = users[obj['id_2']];
-					try {
-						io.sockets.connected[socketUser2['socket']].emit('new delete message', obj );
-					}catch(err){
-					//	console.log(err)
-					}
-				}
-			}
-    	})
-	})
-
-
-	socket.on('sendMsg', (obj)=>{
-		var read = false;
-		var date = new Date();
-		var dateFormated = date.toISOString().substr(0,10);
-		var time = date.toLocaleTimeString();
-		var id_msg;
-		var msgParse = obj['msg'];
-		if(obj['file'] == false){
-			msgParse = urlify(obj['msg']);
-		}
-		var newMessageModel = new msgModel();
-			msgModel.create({
-				id_dialog:obj['id_dialog'],
-				msg: msgParse,
-				id_1:obj['id_1'],
-				id_2:obj['id_2'],
-				user_1:obj['user_1'],
-				user_2:obj['user_1'],
-				id_author:obj['id_author'],
-				time:time,
-				date:dateFormated,
-				read:read,
-				edit:false,
-				file:obj['file']
-			}, function (err, msg) {
-				if (err){ return err}
-				var id_msg = msg.id;
-				var msgObj = {
-					"id_msg":id_msg,
-					"id_dialog":obj['id_dialog'],
-					"msg":msgParse,
-					"id_1":obj['id_1'],
-					"id_2":obj['id_2'],
-					"user_1":obj['user_1'],
-					"user_2":obj['user_1'],
-					"id_author":obj['id_author'],
-					"time":time,
-					"date":dateFormated,
-					"read":false,
-					"edit":false,
-					"file":obj['file']
-				}		
-				socket.emit('getMsg',msgObj);
-				if(users[obj['id_2']]){
-					try {
-						socketUser2 = users[obj['id_2']]
-						/*if( users[obj['id_2']]['id_dialog'] == obj['id_dialog']){read = true;msgObj['read'] = true;}*/
-						io.sockets.connected[socketUser2['socket']].emit('getMsg',msgObj);
-						io.sockets.connected[socketUser2['socket']].emit('newMsgDialog',msgObj);
-						
-					} catch (err) {
-						//console.log(err)
-					}
-				}
-			});
-
-
-	
-		
-
-		
-	})
-
-	socket.on('seen message dialog',(obj)=>{
-
-		msgModel.update({read: false,id_author:obj['id_2'],id_dialog:obj['id_dialog']}, {read: true},{ multi: true }, function(err, result){
-		    if(err) return console.log(err);
-		    if(users[obj['id_2']]){	
-				try {
-					io.sockets.connected[users[obj['id_2']]['socket']].emit('user see you message',{id_dialog:obj['id_dialog']});				
-								
-				} catch (err) {
-					//console.log(err)
-				}
-			}
-			socket.emit('user see you message',{id_dialog:obj['id_dialog']});
-		});
-			
-	})
-
-	socket.on('disconnect',(sock)=>{
-		if(listDisconnect[socket.id_1]){
-			listDisconnect[socket.id_1] = new Date().getTime()
+			})
 		}else{
-			listDisconnect[socket.id_1] = '';
-			listDisconnect[socket.id_1] = new Date().getTime()
+			var sql_update = "UPDATE users SET room = '"+data.url_chat+"' WHERE id ="+data.id_user;
+			con.query(sql_update, function (err2, result2) {
+				if (err2) throw err2;
+				socket.join(data.url_chat);
+				var sql = "SELECT * FROM  chats WHERE url='"+data.url_chat+"'";
+				con.query(sql, function (err, result) {
+					if (err) throw err;
+					socket.emit('server_new_chat_data',{"url_chat":result[0]['url'],"id_chat":result[0]['id'],"id_user":data.id_user,'lang':data.lang})
+				})
+				
+			});
 		}
-		for (var k in listDisconnect) {
-			console.log('user '+k+' last visit '+timeSince(listDisconnect[k]))
+	})
+	socket.on('client_change_lang',(data)=>{
+		if(data.id_user != 'undefined'){
+			var sql_update = "UPDATE users SET lang = '"+data.lang+"' WHERE id ="+data.id_user;
+			con.query(sql_update, function (err2, result2) {
+				if (err2) throw err2;
+			});
+			
 		}
-		delete users[listSocket[socket.id]]
+	})
+	socket.on('client_load_chat_msg',(data)=>{
+		if(data.id_chat != 'undefined'){
+			var sql = "SELECT * FROM  message WHERE chat_id='"+data.id_chat+"'";
+			con.query(sql, function (err, result) {
+				if (err) throw err;
+				socket.emit('server_get_chat_msg',result)
+			});
+		}
+	})
+	socket.on('client_send_new_msg',(data)=>{
+		//socket.emit('test_emit',socket)
+		var sql = "SELECT * FROM  users WHERE room="+data.url_chat;
+			con.query(sql, function (err, result) {
+			if (err) throw err;
+			if(result.length > 0){
+				var pr = new Promise(function (resolve, reject) {
+					var objMsg = {};
+					var count = 1;
+					for (var k in result) {
+						const lang = result[k]['lang']
+						googleTranslate.translate(data.msg, lang, function(err, translation){
+							if (err) throw err;
+							objMsg[lang] = translation.translatedText;
+							if(count == result.length ){
+								resolve(objMsg)
+							}
+							count++;
+						})
+
+					}
+				});
+
+				pr.then(function(obj){
+					io.sockets.in(data.url_chat).emit('server_new_msg',{"date":data.date,"msg_from":data.id_user,"msg":obj});
+					var sql = "INSERT INTO message (chat_id,msg_from,msg,date) VALUES ('"+data.url_chat+"','"+data.id_user+"','"+JSON.stringify(obj)+"','"+data.date+"')";
+					con.query(sql, function (err, result) {
+						if (err) throw err;
+					});
+
+				})	
+			}
+		});
+	})
+	socket.on('disconnect',(data)=>{
+
+		/*var sql = "SELECT * FROM  chats WHERE users LIKE '%" + socket.id + "%'";
+			con.query(sql, function (err, result) {
+				if (err) throw err;
+				if(result.length > 0){
+					load_chat_data = result;
+					var users = JSON.parse(result[0]['users']);
+						users.splice(users.indexOf(socket.id),1)
+						users = JSON.stringify(users);
+					var sql_update = "UPDATE chats SET users = '"+users+"' WHERE url ='"+result[0].url+"'";
+					con.query(sql_update, function (err_2, result_2) {});
+				}
+			});*/
 	})
 });
-
-function timeSince(date) {
-	  var seconds = Math.floor((new Date().getTime() - date)/1000);
-	  var interval = Math.floor(seconds / 31536000);
-	  if (interval > 1) {
-	    return interval + " years";
-	  }
-	  interval = Math.floor(seconds / 2592000);
-	  if (interval > 1) {
-	    return interval + " months";
-	  }
-	  interval = Math.floor(seconds / 86400);
-	  if (interval > 1) {
-	    return interval + " days";
-	  }
-	  interval = Math.floor(seconds / 3600);
-	  if (interval > 1) {
-	    return interval + " hours";
-	  }
-	  interval = Math.floor(seconds / 60);
-	  if (interval >= 1) {
-	    return interval + " minutes";
-	  }
-	  return Math.floor(seconds) + " seconds";
-}
-
-function urlify(text) {
-    var urlRegex = /(https?:\/\/[^\s]+)/g;
-    return text.replace(urlRegex, function(url) {
-        return '<a target="blank" href="' + url + '">' + url + '</a>';
-    })
-}
 
